ce15deed25857d0e4047e15d805bc31783d55c5e
Laifsyn <laifzing@gmail.com>
2024-10-14 17:34:52 -0500

    Draw Edge Weights

--- python/estructura_datos/Dijkstra.py
+++ python/estructura_datos/Dijkstra.py
@@ -1,43 +1,135 @@
-# from typing import Callable
-# from manim import *
+from typing import Any, Callable, OrderedDict, Tuple
+from manim import *
+from manim.typing import Point3D
 
 
-# class TestPy(Scene):
-#     def construct(self):
-#         vertices = [i for i in range(7)]
-#         edges = [
-#             (0, 1),
-#             (1, 2),
-#             (3, 2),
-#             (3, 4),
-#             (5, 2),
-#         ]
-
-#         edge_config = {
-#             "stroke_width": 2,
-#             "tip_config": {
-#                 "tip_shape": StealthTip,
-#                 "tip_length": 0.15,
-#             },
-#             (3, 4): {
-#                 "color": RED,
-#                 "tip_config": {"tip_length": 0.25, "tip_width": 0.25}
-#             },
-#         }
-
-#         g = DiGraph(
-#             vertices,
-#             edges,
-#             labels=True,
-#             layout="circular",
-#             edge_config=edge_config,
-#         ).scale(1.4)
-
-#         self.play(Create(g))
-#         self.wait()
+from vertex import Dijkstra, GraphMap, Vertex
 
 
-# def graph_map():
-    
+class Manim_Dijkstra(Scene):  # type: ignore
+    def __init__(self, random: Any = 0):
+        super().__init__()
+        dijkstra, steps = dijkstra_func_1()
+        self.dijkstra: Dijkstra = dijkstra
+        self.steps: int = steps
+        # TODO Undo debug settings
+        config.frame_rate = 24
+        self.animate_drawing_weighted_edges = False  # Default: True
 
-# TestPy().render(preview=True)
+    def draw_weigthed_edges(self) -> Any:
+        # Weights Setup: Get the midpoint between vertexes
+        positions: OrderedDict[Tuple[Vertex, Vertex], Point3D] = OrderedDict()
+        for from_v, to_v in self.dijkstra.graph.get_edges():
+            pos1 = self.dijkstra.graph.get_vertex_position(from_v)
+            pos2 = self.dijkstra.graph.get_vertex_position(to_v)
+            pos = ((pos1[0] + pos2[0]) / 2, (pos1[1] + pos2[1]) / 2, 0.0)
+            # Sort the vertex index
+            if from_v.id > to_v.id:
+                from_v, to_v = to_v, from_v
+            positions.update({(from_v, to_v): pos})
+
+        # Instantiate Animations
+        animations: list[Tuple[Rectangle, Tex]] = list()
+        for (from_v, to_v), pos in positions.items():
+            # find the matching edge in the graph
+            for edge in self.dijkstra.graph.edges:
+                if edge.from_v == from_v and edge.to_v == to_v:
+                    weight = edge.weight
+                    break
+            else:
+                raise Exception("Unexpected case : Edge not found")
+            weight_background = (
+                Rectangle(width=0.5, height=0.5, fill_color=BLACK, fill_opacity=1)
+                .move_to(pos)
+                .set_stroke(GREEN_A, opacity=0.7)
+            )
+            weight = Tex(str(weight)).move_to(pos).set_color(TEAL_E)
+            animations.append((weight_background, weight))
+
+        # Commit the Animations
+        if self.animate_drawing_weighted_edges:
+            animations = [
+                AnimationGroup(FadeIn(rect), Create(anim))
+                for (rect, anim) in animations
+            ]
+            self.play(LaggedStart(*animations, lag_ratio=0.3), run_time=3)
+            del animations
+        else:
+            # Flatten the tuple
+            animations = [
+                anim for anims in animations for anim in anims
+            ]  # If you praise me, or judge me for this: Copilot suggested this.
+            self.add(*animations)
+
+    def construct(self):
+        vertices = self.dijkstra.graph.get_vertexes_ids()
+        edges = [(to_v, from_v) for from_v, to_v in self.dijkstra.graph.get_edges()]
+        vertices_layout = self.dijkstra.graph.get_vertexes_layout()
+
+        edge_config = {
+            "stroke_width": 2,
+            "tip_config": {
+                "tip_shape": StealthTip,
+                "tip_length": 0.15,
+            },
+            (Vertex(1), Vertex(3)): {
+                "color": BLUE_E,
+            },
+        }
+
+        g = Graph(
+            vertices,
+            edges,
+            labels=True,
+            layout=vertices_layout,
+            edge_config=edge_config,
+        ).scale(1)
+        
+        self.camera.frame_center = g.get_center()
+        self.play(Create(g))
+        self.draw_weigthed_edges()
+        self.wait()
+
+
+def dijkstra_func_1() -> Tuple[Dijkstra, int]:
+    # Should be the map below
+    # https://www.youtube.com/watch?v=EFg3u_E6eHU
+    map = GraphMap()
+    vertexes: list[Tuple[Vertex, Tuple[float, float]]] = [
+        (Vertex(1), (0, 0)),
+        (Vertex(3), (1, 1.5)),
+        (Vertex(6), (1, -1)),
+        (Vertex(5), (2, 0)),
+        (Vertex(4), (3, 1.5)),
+        (Vertex(7), (3, -1.2)),
+        (Vertex(2), (4, 0)),
+    ]
+    vertexes = [(v, (x * 2, y * 2)) for v, (x, y) in vertexes]
+    map.register_vertexes(vertexes)
+
+    edges = [
+        (3, 1, 3),
+        (2, 1, 6),
+        (2, 3, 6),
+        (1, 3, 5),
+        (3, 6, 5),
+        (5, 6, 7),
+        (6, 6, 2),
+        (2, 5, 2),
+        (4, 3, 4),
+        (1, 4, 2),
+        (2, 7, 2),
+    ]
+    for weight, _from, to in edges:
+        map.define_edge(weight, _from, to, True)
+
+    # map.debug()
+    dijkstra = Dijkstra.new(map, 1, 2)
+    steps = 30  # Some arbitrary step amount
+    if dijkstra is None:
+        raise Exception("Map is None")
+
+    return (dijkstra, steps)
+
+
+Manim_Dijkstra().render(True)
--- python/estructura_datos/vertex.py
+++ python/estructura_datos/vertex.py
@@ -1,5 +1,16 @@
 from enum import Enum
-from typing import Any, Dict, Generator, List, Optional, OrderedDict, Self, Set, Tuple
+from typing import (
+    Any,
+    Dict,
+    Generator,
+    List,
+    Optional,
+    OrderedDict,
+    Self,
+    Set,
+    Tuple,
+    TypeAlias,
+)
 
 # https://www.youtube.com/watch?v=EFg3u_E6eHU
 
@@ -8,9 +19,40 @@ VertexId = int
 
 class Vertex:
 
-    def __init__(self, id: int):
+    int_to_char = [
+        "0",
+        "A",
+        "B",
+        "C",
+        "D",
+        "E",
+        "F",
+        "G",
+        "H",
+        "I",
+        "J",
+        "K",
+        "L",
+        "M",
+        "N",
+        "O",
+        "P",
+        "Q",
+        "R",
+        "S",
+        "T",
+        "U",
+        "V",
+        "W",
+        "X",
+        "Y",
+        "Z",
+    ]
+
+    def __init__(self, id: int, repr_as_int: bool = False):
         self.id: VertexId = id
         self.status = ExplorationStatus.UNEXPLORED
+        self.repr_as_int = repr_as_int
 
     def explore(self):
         self.status = ExplorationStatus.EXPLORED
@@ -25,16 +67,27 @@ class Vertex:
     def __hash__(self) -> int:
         return hash(self.id)
 
+    def __str__(self) -> str:
+        char = Vertex.int_to_char[self.id]
+        if char is None or self.repr_as_int:
+            return f"{self.id}"
+        return char
+
+    @staticmethod
+    def int_to_char_dict() -> Dict[int, str]:
+        return {i: Vertex.int_to_char[i] for i in range(len(Vertex.int_to_char))}
+
+
 class ExplorationStatus(Enum):
     EXPLORED = 1
     UNEXPLORED = 0
 
-EdgeWeight = int
+
+EdgeWeight: TypeAlias = int
+
 
 class Edge:
     def __init__(self, weight: EdgeWeight, from_v: Vertex, to_v: Vertex):
-        # TODO Remove debug print
-        print(f"Creating [{from_v.id}][{to_v.id}] = {weight}")
         self.weight = weight
         self.from_v: Vertex = from_v
         self.to_v: Vertex = to_v
@@ -81,25 +134,27 @@ class GraphMap:
         return
 
     # Adds an edge to the graph. Returns early when the vertex's id is missing
-    def define_edge(self, weigth: int, id_from: int, id_to: int) -> Optional[Edge]:
+    def define_edge(
+        self, weigth: int, id_from: int, id_to: int, is_bidirected=False
+    ) -> Optional[Edge]:
         if id_from == id_to:
             return None
         vertex_from = None
         vertex_to = None
+        # Check if the vertex exists in the Graph
         for vertex in self.vertices.values():
             if vertex.id == id_from:
                 vertex_from = vertex
             elif vertex.id == id_to:
                 vertex_to = vertex
-
-        if vertex_from is None:
-            return None
-        if vertex_to is None:
+        if vertex_from is None or vertex_to is None:
             return None
 
         edge = Edge(weigth, vertex_from, vertex_to)
 
         self.edges.add(edge)
+        if is_bidirected:
+            self.edges.add(Edge(weigth, vertex_to, vertex_from))
         return edge
 
     def get_adjacent_to(self, src: Vertex) -> Optional[list[Tuple[Vertex, EdgeWeight]]]:
@@ -109,11 +164,42 @@ class GraphMap:
         # search for the edges that have the `src` vertex as the starting point
         for edge in self.edges:
             if edge.from_v.id == src.id:
-                adjacent_vertexes.append((edge.to_v, 0))
+                adjacent_vertexes.append((edge.to_v, edge.weight))
 
         if len(adjacent_vertexes) > 0:
             return adjacent_vertexes
         return None
+
+    def get_vertexes_ids(self) -> List[Vertex]:
+        return [
+            vertex
+            for vertex in sorted(list(self.vertices.values()), key=lambda x: x.id)
+        ]
+
+    def get_edges(self) -> List[Tuple[Vertex, Vertex]]:
+        return [(edge.from_v, edge.to_v) for edge in self.edges]
+
+    def get_vertexes_layout(self) -> Dict[Vertex, Tuple[float, float, float]]:
+        layout = dict()
+        for coords, vertex in self.vertices.items():
+            layout.update({vertex: (coords[0], coords[1], 0.0)})
+        return layout
+
+    def get_vertex_position(self, v: Vertex) -> Coords:
+        for coords, vertex in self.vertices.items():
+            if vertex == v:
+                return coords
+        raise ValueError("Vertex not found")
+
+    # TODO: Delete this method
+    def get_center(self, v1: Vertex, v2: Vertex) -> Tuple[float, float]:
+        # get the coords for the vertex
+        coords: list[Coords] = [
+            self.get_vertex_position(v1),
+            self.get_vertex_position(v2),
+        ]
+        return tuple((a + b) / 2 for a, b in zip(*coords))
+        pass
 
     def debug(self):
         # print vertexes
@@ -123,6 +209,7 @@ class GraphMap:
             print(f"  ({coords[0]:3}, {coords[1]:3}) - id:{vertex.id:2}")
         # Amount of vertices:
         vertices = list(self.vertices.values())
+        vertices = sorted(vertices, key=lambda x: x.id)
         num_vertices = len(vertices)
         if num_vertices > 0:
             # Create an empty adjacency matrix filled with "zeros"
@@ -137,7 +224,7 @@ class GraphMap:
             print("Matriz de Adyacencia:")
 
             # Get the vertex IDs as labels
-            vertex_ids = [vertex.id for vertex in vertices]
+            vertex_ids = [vertex.__str__() for vertex in vertices]
 
             # Print the header (vertex IDs)
             print("    " + "  ".join(f"{vid:2}" for vid in vertex_ids))
@@ -154,23 +241,36 @@ class GraphMap:
 class Dijkstra:
 
     @staticmethod
-    def new(graph: GraphMap, start_vertex: Vertex | VertexId) -> Optional["Dijkstra"]:
+    def new(
+        graph: GraphMap,
+        start_vertex: Vertex | VertexId,
+        target_vertex: Vertex | VertexId,
+    ) -> Optional["Dijkstra"]:
         if isinstance(start_vertex, Vertex):
             start_vertex = start_vertex.id
+        if isinstance(target_vertex, int):
+            target_vertex = Vertex(target_vertex)
 
         for vertex in graph.vertices.values():
             # Find if the starting vertex exists
             if vertex.id == start_vertex:
                 start_vertex = vertex
+            if vertex.id == target_vertex.id:
+                target_vertex = vertex
             # Mark vertexes as unexplored
             vertex.status = ExplorationStatus.UNEXPLORED
 
         if isinstance(start_vertex, int):
             return None
-        return Dijkstra(graph, start_vertex, I_KNOW_WHAT_IM_DOING=True)
+
+        return Dijkstra(graph, start_vertex, target_vertex, I_KNOW_WHAT_IM_DOING=True)
 
     def __init__(
-        self, graph: GraphMap, start_vertex: Vertex, I_KNOW_WHAT_IM_DOING=False
+        self,
+        graph: GraphMap,
+        start_vertex: Vertex,
+        target_vertex: Vertex,
+        I_KNOW_WHAT_IM_DOING=False,
     ) -> None:
         if not I_KNOW_WHAT_IM_DOING:
             raise ValueError(
@@ -182,41 +282,54 @@ class Dijkstra:
             self.vertex_weight.update({vertexes: None})
 
         self.vertex_weight[start_vertex] = 0
+        self.target_vertex = target_vertex
         self.return_vertex: Dict[Vertex, Vertex | None] = dict()
-        self.solution = self.gen_solution()
+        self.solution = self._advance()
         self.steps: int = 0
 
     def advance(self, steps: int = 1, debug: bool = False):
         for _ in range(steps):
             self.steps += 1
-            next(self.solution, None)
+            if next(self.solution, None) is None:
+                break
             if debug:
                 print(f"Debug Step: {self.steps}")
                 self.debug()
 
-    def gen_solution(self) -> Generator[int, None, None]:
+    def _advance(self) -> Generator[int, None, None]:
         self.shortest_path = None
         start_vertex = self.get_next_smallest()
         # vertex_queue = [start_vertex]
-        while self.shortest_path == None:
+        while True:
             neighbour_nodes = self.get_adjacent(start_vertex)
             # Mark as explored
             start_vertex.explore()
             if neighbour_nodes is None:
-                raise ValueError("TODO:No adjacent vertexes found")
+                break
 
             # neighbour, weight_to_neighbour
             for neighbour, weight_to in neighbour_nodes:
                 weight_till_neighbour = self.vertex_weight[neighbour]
                 this_weight = self.get_weight(start_vertex)
-                if (
-                    weight_till_neighbour is None
-                    or this_weight + weight_to < weight_till_neighbour
+                if (weight_till_neighbour) is None or (
+                    this_weight + weight_to < weight_till_neighbour
                 ):
                     self.vertex_weight[neighbour] = this_weight + weight_to
                     self.return_vertex[neighbour] = start_vertex
+                    print(
+                        f"Updating:{self.vertex_weight[neighbour]} <= {this_weight} + {weight_to}"
+                    )
+                else:
+                    print("No changes???")
                 yield 0
             start_vertex = self.get_next_smallest()
+        print(f"End of the line {self.steps}")
+
+    def get_start_vertex(self) -> Vertex:
+        for v, w in self.vertex_weight.items():
+            if w == 0:
+                return v
+        raise ValueError("No starting vertex found")
 
     def get_next_smallest(self) -> Vertex:
         # Get the next smallest vertex that isn't explored
@@ -269,89 +382,135 @@ class Dijkstra:
         for this, to in self.return_vertex.items():
             t = "None"
             if to is not None:
-                t = str(to.id)
-            print(f"{this.id}({self.vertex_weight[this] :3})->{t}")
+                t = str(to)
+            print(f"{this}({self.vertex_weight[this] :3})->{t}")
+
         print("\n")
 
 
-def main():
-    map = GraphMap()
-    map.register_vertexes(
-        [
-            (Vertex(1), (0, 0)),
-            (Vertex(2), (1, 1)),
-            (Vertex(3), (1, -1)),
-            (Vertex(4), (2, 1)),
-            (Vertex(5), (2, -1)),
-            (Vertex(6), (3, 1)),
-            # (Vertex(7), (3, -1)),
-            # (Vertex(8), (4, 0)),
-        ]
-    )
-    edges = [
-        (1, 2),
-        (1, 4),
-        (2, 1),
-        (2, 4),
-        (2, 5),
-        (4, 1),
-        (4, 2),
-        (4, 5),
-        (4, 6),
-        (5, 2),
-        (5, 4),
-        (5, 6),
-        (5, 3),
-        (6, 5),
-        (6, 4),
-        (6, 3),
-        (3, 5),
-        (3, 6),
-    ]
-    for i in range(len(edges)):
-        map.define_edge(i + 1, edges[i][0], edges[i][1])
-    map.debug()
+# def main():
+#     map = GraphMap()
+#     map.register_vertexes(
+#         [
+#             (Vertex(1), (0, 0)),
+#             (Vertex(2), (1, 1)),
+#             (Vertex(3), (1, -1)),
+#             (Vertex(4), (2, 1)),
+#             (Vertex(5), (2, -1)),
+#             (Vertex(6), (3, 1)),
+#             # (Vertex(7), (3, -1)),
+#             # (Vertex(8), (4, 0)),
+#         ]
+#     )
+#     edges = [
+#         (1, 2),
+#         (1, 4),
+#         (2, 1),
+#         (2, 4),
+#         (2, 5),
+#         (4, 1),
+#         (4, 2),
+#         (4, 5),
+#         (4, 6),
+#         (5, 2),
+#         (5, 4),
+#         (5, 6),
+#         (5, 3),
+#         (6, 5),
+#         (6, 4),
+#         (6, 3),
+#         (3, 5),
+#         (3, 6),
+#     ]
+#     for i in range(len(edges)):
+#         map.define_edge(i + 1, edges[i][0], edges[i][1])
+#     map.debug()
 
-    dijkstra = Dijkstra.new(map, 3)
-    # TODO Remove debug print
-    print("Advancing:")
-    dijkstra.advance(5, True)
-
-
-main()
+#     dijkstra = Dijkstra.new(map, 3)
+#     # TODO Remove debug print
+#     print("Advancing:")
+#     dijkstra.advance(5, True)
 
 
-map = GraphMap()
-map.register_vertexes(
-    [
-        (Vertex(1), (0, 0)),
-        (Vertex(2), (1, 1)),
-        (Vertex(4), (1, -1)),
-        (Vertex(5), (2, 1)),
-        (Vertex(6), (2, -1)),
-        (Vertex(3), (3, 1)),
-    ]
-)
-edges = [
-    (2, 1, 2),
-    (8, 1, 4),
-    (2, 2, 1),
-    (5, 2, 4),
-    (6, 2, 5),
-    (8, 4, 1),
-    (5, 4, 2),
-    (3, 4, 5),
-    (2, 4, 6),
-    (6, 5, 2),
-    (3, 5, 4),
-    (1, 5, 6),
-    (9, 5, 3),
-    (1, 6, 5),
-    (2, 6, 4),
-    (3, 6, 3),
-    (9, 3, 5),
-    (3, 3, 6),
-]
-for i in edges:
-    map.define_edge(i[0], i[1], i[2])
-map.debug()
+# # main()
+# def main_2():
+#     map = GraphMap()
+#     map.register_vertexes(
+#         [
+#             (Vertex(1), (0, 0)),
+#             (Vertex(2), (1, 1)),
+#             (Vertex(4), (1, -1)),
+#             (Vertex(5), (2, 1)),
+#             (Vertex(6), (2, -1)),
+#             (Vertex(3), (3, 1)),
+#         ]
+#     )
+#     edges = [
+#         (2, 1, 2),
+#         (8, 1, 4),
+#         (2, 2, 1),
+#         (5, 2, 4),
+#         (6, 2, 5),
+#         (8, 4, 1),
+#         (5, 4, 2),
+#         (3, 4, 5),
+#         (2, 4, 6),
+#         (6, 5, 2),
+#         (3, 5, 4),
+#         (1, 5, 6),
+#         (9, 5, 3),
+#         (1, 6, 5),
+#         (2, 6, 4),
+#         (3, 6, 3),
+#         (9, 3, 5),
+#         (3, 3, 6),
+#     ]
+#     for weight, _from, to in edges:
+#         map.define_edge(weight, _from, to, True)
+#     map.debug()
+
+#     dijkstra = Dijkstra.new(map, 1, 3)
+#     if dijkstra is None:
+#         print("Dijkstra is None")
+#         return
+#     # TODO Remove debug print
+#     print("Advancing:")
+#     dijkstra.advance(15, True)
+
+
+# def main_1():
+#     # https://www.youtube.com/watch?v=EFg3u_E6eHU
+#     map = GraphMap()
+#     map.register_vertexes(
+#         [
+#             (Vertex(1), (0, 0)),
+#             (Vertex(3), (1, 2)),
+#             (Vertex(6), (1, -1)),
+#             (Vertex(5), (2, 1)),
+#             (Vertex(4), (3, 3)),
+#             (Vertex(7), (3, -1)),
+#             (Vertex(2), (4, 1)),
+#         ]
+#     )
+#     edges = [
+#         (3, 1, 3),
+#         (2, 1, 6),
+#         (2, 3, 6),
+#         (1, 3, 5),
+#         (3, 6, 5),
+#         (5, 6, 7),
+#         (6, 6, 2),
+#         (2, 5, 2),
+#         (4, 3, 4),
+#         (1, 4, 2),
+#         (2, 7, 2),
+#     ]
+#     for weight, _from, to in edges:
+#         map.define_edge(weight, _from, to, True)
+
+#     map.debug()
+#     Dijkstra.new(map, 1, 2).advance(30, True)
+#     print("Dijkstra at main_1()")
+
+
+# main_1()
